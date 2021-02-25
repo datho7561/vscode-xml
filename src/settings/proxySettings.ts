4c766306a89193c1f5aac25448e97423d6c57127
David Thompson <davthomp@redhat.com>
2021-02-25 17:08:40 -0500

    Downloading binary server works through a proxy
    Work in progress

--- src/settings/proxySettings.ts
+++ src/settings/proxySettings.ts
@@ -1,14 +1,52 @@
+import * as http from 'http';
 import { workspace } from "vscode";
 
-/**
- *
- * @returns The prefix to append to URLs
- */
-export function getProxyPrefix(): string {
-  const proxy : string = workspace.getConfiguration('http').get('proxy', undefined);
-  const proxyAuthorization : string = workspace.getConfiguration('http').get('proxyAuthorization', undefined);
-  if (!proxy || !proxyAuthorization) {
-    return '';
+export namespace ProxySettings {
+
+  /**
+   * Returns the address of the proxy
+   *
+   * @returns the address of the proxy
+   */
+  function getProxyAddress(): string {
+    return workspace.getConfiguration('http').get('proxy', undefined);
+  }
+
+  /**
+   * Returns the Proxy-Authorization to use to access the proxy
+   *
+   * @returns The Proxy-Authorization to use to access the proxy
+   */
+  function getProxyAuthorization(): string {
+    return workspace.getConfiguration('http').get('proxyAuthorization', undefined);
+  }
+
+  /**
+   * Returns the http get options for the configured proxy, or undefined if no proxy is set up
+   *
+   * @returns the http get options for the configured proxy, or undefined if no proxy is set up
+   */
+  export function getHttpGetOptionsForProxy(): http.RequestOptions {
+    const proxyAddress: string = getProxyAddress();
+    if (!proxyAddress) {
+      return undefined;
+    }
+    const portSplitResult: string[] = proxyAddress.split(":");
+    const proxyHost: string = portSplitResult[0];
+    const proxyPort: string = portSplitResult[1];
+    if (!proxyHost || !proxyPort) {
+      return undefined;
+    }
+    let proxyAuth: string = getProxyAuthorization();
+    proxyAuth = !proxyAuth ? undefined : proxyAuth;
+
+    return {
+      headers: {
+        'Proxy-Authorization': proxyAuth
+      },
+      host: proxyHost,
+      port: proxyPort
+    }
   }
 
 }
